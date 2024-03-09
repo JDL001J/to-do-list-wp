14e1af09122bcfb7664ceb0ca22fc7deaf364b0c
JDL001J <116929245+JDL001J@users.noreply.github.com>
2024-03-08 18:17:37 -0600

    add dialog_box and showmodal

--- src/dialog_box.js
+++ src/dialog_box.js
@@ -1,13 +1,37 @@
+// import { createItem } from "./create_item";
+
 import { createItem } from "./create_item";
 
 export const dialogBox = () => {
     
     const box = document.createElement('dialog');
     box.setAttribute('id', 'dialog-box');
-    const body = document.querySelector('#body')
+    const form = document.createElement('form');
+    const inputTitle = document.createElement("input");
 
-    body.appendChild(box);
-    box.showModal()
+    const submit = document.createElement("button");
+    submit.setAttribute('type', 'submit');
+    submit.textContent = "Submit"
+    form.appendChild(submit)
+
+
+
+form.appendChild(inputTitle);
+box.appendChild(form);
+
+
+   const body = document.querySelector("#body");
+
+
+   body.appendChild(box)
+
+   document.querySelector("form").addEventListener("submit", (e) => {
+    e.preventDefault();
+    createItem()
+  });
+
+   
+box.show()
 
 
 }
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 
 import {onLoad} from './onLoad.js';
-
+import {dialogBox} from './dialog_box';
 
 
 onLoad();
-
+dialogBox()
